42a3771921a2a6794c9317e3703cec15ea761257
Reda Saad <Reda.Saad@gmail.com>
2025-04-14 03:18:18 +0200

    commit

---
+++ Api/Program.cs
@@ -0,0 +1,131 @@
+
+using Core.Interfaces;
+using Core.Models;
+using Core.Servises;
+using Core.Settings;
+using Infrastructure;
+using Infrastructure.Authentication;
+
+using Infrastructure.UnitOfWork;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
+
+namespace ProjectApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDBContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"),
+                    options => options.MigrationsAssembly(typeof(AppDBContext).Assembly.FullName));
+            });
+            builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection("jwt"));
+
+            builder.Services.AddTransient(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+
+
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>().
+                AddEntityFrameworkStores<AppDBContext>();
+
+            builder.Services.AddScoped<PasswordHasher<AppUser>>();
+            builder.Services.AddTransient<Service>();
+
+            builder.Services.AddTransient<IAuthentication, Authentication>();
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["jwt:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwt:Key"]))
+
+                };
+
+            });
+
+
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddCors();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+
+                });
+
+                options.AddSecurityRequirement(securityRequirement: new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+
+                    }
+
+                });
+
+            });
+
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.UseStaticFiles();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Core/Models/DetailsProperty.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class DetailsProperty
+    {
+        [Key]
+        public string Id { get; set; }
+        [ForeignKey("PropertyId")]
+        public Property Property { get; set; }
+        public string PropertyId { get; set; }
+
+
+
+    }
+}
