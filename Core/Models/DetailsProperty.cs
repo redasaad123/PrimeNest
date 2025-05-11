5f22367892d05e15de40b15ecaabe70c70f529b7
Reda Saad <Reda.Saad@gmail.com>
2025-05-11 03:45:35 +0300

    first commit

---
+++ Core/Models/DetailsProperty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public abstract class DetailsProperty
+    {
+        [Key]
+        public string Id { get; set; }
+        [ForeignKey("PropertyId")]
+        public string Type { get; set; }
+        public string PropertyId { get; set; }
+        [JsonIgnore]
+        public Property Property { get; set; }
+
+
+
+    }
+}
---
+++ ProjectApi/Controllers/PropertyController.cs
@@ -0,0 +1,117 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using Core.Servises;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProjectApi.DTOS;
+
+using ProjectApi.NewFolder;
+
+namespace ProjectApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertyController : ControllerBase
+    {
+        private readonly Service service;
+        private readonly IUnitOfWork<Property> propertyUnitOfWork;
+        private readonly UserManager<AppUser> userManager;
+
+        public PropertyController(Service service, IUnitOfWork<Property> PropertyUnitOfWork, UserManager<AppUser> userManager)
+        {
+            this.service = service;
+            propertyUnitOfWork = PropertyUnitOfWork;
+            this.userManager = userManager;
+        }
+
+        [HttpGet("GetAllProperty")]
+
+        public async Task<IActionResult> GetAllProperty()
+        {
+            var Property = await propertyUnitOfWork.Entity.GetAllAsync();
+            if (Property.Count() == 0)
+                return NotFound("Not Found Any Property");
+
+            var map = Property.SkipWhile(x => x.IsRequested == true).Select(x => new GetPropertyDTO
+            {
+                Id = x.Id,
+                OwnerName = userManager.Users.Where(u => u.Id == x.user).Select(u => u.Name).FirstOrDefault() ?? x.user,
+                TypeContract = x.TypeContract,
+                Type = x.Type,
+                Address = x.Address,
+                Area = x.Area,
+                date = x.date,
+                Description = x.Description,
+                MainPhoto = x.MainPhoto,
+                MoreDescription = x.MoreDescription,
+                Photo = x.Photo,
+                Price = x.Price,
+                State = x.State ? "Availabe" : "Notavailabe"
+
+
+            });
+            return Ok(map);
+
+        }
+
+        [HttpPost("Search")]
+        public async Task< IActionResult> search([FromForm]PropertySearchDto dto)
+        {
+            var result = await service.SearchProperties(dto);
+            return Ok(result);
+        }
+
+        [HttpPost("AddProperty")]
+        public async Task<IActionResult> AddProperty(AddPropertyDTO dto )
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var user = await userManager.GetUserAsync(User);
+            var property = new Property();
+            property.Id = Guid.NewGuid().ToString();
+
+            var resalt = await service.Help(property, dto, user);
+            if (resalt == null)
+                return BadRequest();
+
+            await propertyUnitOfWork.Entity.AddAsync(property);
+            propertyUnitOfWork.Save();
+            return Ok(resalt);
+        }
+
+
+        [HttpPut("UpdateProperty/{propertyId}")]
+        public async Task<IActionResult> UpdateProperty(string propertyId, UpdatePropertyDTO dto)
+        {
+            var property = await propertyUnitOfWork.Entity.GetAsync(propertyId);
+            if (property == null)
+                return BadRequest();
+            var user = await userManager.GetUserAsync(User);
+
+            var resalt = await service.Help(property, dto, user);
+            if (resalt == null)
+                return BadRequest();
+
+            await propertyUnitOfWork.Entity.UpdateAsync(property);
+            propertyUnitOfWork.Save();
+            return Ok(resalt);
+
+        }
+
+        [HttpDelete("DeleteProperty/{propertyId}")]
+        public async Task<IActionResult> DeleteProperty(string propertyId)
+        {
+            var property = await propertyUnitOfWork.Entity.GetAsync(propertyId);
+            if (property == null)
+                return BadRequest();
+
+            propertyUnitOfWork.Entity.Delete(property);
+            propertyUnitOfWork.Save();
+            return Ok(property);
+        }
+
+
+    }
+}
